8d71c93f43dc6b7a3f5ed2a31918c98713c4dbc0
Iván García Rodríguez <ivan.garcia.rodriguez@hotmail.es>
2022-10-11 23:16:13 +0200

    TODO#8 Done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 //DONE: #1 implementar api (getGames(), getGameById()). Mirar en la documentación de json-server
 // DONE: #7 Separar api de index.js
 import {getGames, getGameById, getCommentsOfGame} from "./api-handler.js";
-// TODO: #8 Separar funciones auxiliares `(generate*)` de renders en index.js
+// DONE: #8 Separar funciones auxiliares `(generate*)` de renders en index.js
+import { generateGameSnippet, generateCommentSnippet } from "./functions.js";
 // TODO: #9 Separar funciones de index y de detail
 
 
@@ -57,39 +58,3 @@ export async function drawComments(gameId) {
   }
 }
 
-// DONE: #4 Dar más información que una lista (imagen, nombre, primeros 100 caracteres de summary)
-function generateGameSnippet(game) {
-  return `
-  <div class="game-snippet">
-    <a href="/detail.html?id=${game.id}">
-      <div class="game-image-container">
-        <img class="img-thumbnail" id="game-${game.id}" src="${game.image}" alt="${game.name}"/>
-      </div>
-      <h3 class="game-name">${game.name}</h3>
-    </a>
-  </div>
-  `;
-}
-
-function generateCommentSnippet(comment) {
-  return `
-  <div class="row">
-  <div class="col col-2">
-    <div class="image-container text-right">
-      <img class="avatar rounded" src="${comment.user.image}" alt="${comment.user.name}" />
-    </div>
-  </div>
-  <div class="col col-10">
-    <div class="user-name-container">
-      <h4 class="text-muted">${comment.user.name}</h4>
-    </div>
-    <div class="comment-body-container">
-      ${comment.body}
-    </div>
-    <div class="date-container">
-      Comment date: <b>${comment.commentDate}</b>
-    </div>
-  </div>
-</div>
-  `
-}
